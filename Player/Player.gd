7b0410d5ffc61b225374e382f3196df0b1fcd468
Adrian S <AdrianSima@users.noreply.github.com>
2019-10-05 14:55:14 +0300

    project creation

---
+++ Player/Player.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+export var HP: int = 100
+export var POWER: int = 50
+export var SPEED: float = 1.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
