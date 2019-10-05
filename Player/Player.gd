ee70db2d7ffd7c36a3b02376dd91a5a817c2e725
Adrian S <AdrianSima@users.noreply.github.com>
2019-10-05 18:34:58 +0300

    basic movement

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,13 +1,44 @@
-extends Node2D
+extends KinematicBody2D
 
-export var HP: int = 100
-export var POWER: int = 50
-export var SPEED: float = 1.0
+export (int) var HP: int = 100
+export (int) var POWER: int = 50
+export (float) var SPEED: float = 1.0
+export (float) var FRICTION: float = 0.05
+
+var rotation_speed = 1.2
+var move_speed = 200
+
+var acceleration_velocity = Vector2()
+var velocity = Vector2()
+var rotation_dir = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func get_input():
+	rotation_dir = 0
+	velocity = Vector2()
+	if Input.is_action_pressed('rotate_right'):
+		rotation_dir += 1
+	if Input.is_action_pressed('rotate_left'):
+		rotation_dir -= 1
+	if Input.is_action_pressed('brake'):
+		acceleration_velocity = Vector2(-move_speed, 0).rotated(rotation)
+	if Input.is_action_pressed('accelerate'):
+		acceleration_velocity = Vector2(move_speed, 0).rotated(rotation)
+		
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().quit()
+	
+
+func _physics_process(delta):
+	get_input()
+	rotation += rotation_dir * rotation_speed * delta
+	velocity += acceleration_velocity
+	velocity *= FRICTION
+	velocity = move_and_slide(velocity)
+
+
+
+
