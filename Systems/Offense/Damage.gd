e020ba17b074bdde90a825980c6e41dcf32f73bb
AdrianSima <simaadriannicolae@gmail.com>
2019-10-06 12:34:06 +0300

    still early work on asteroids
    - moved the level warp code into a separate node
    - added FlakCannon damage
    - removed Damage.gd because it was a left over since before we created the TakesDamage node

--- Systems/Offense/Damage.gd
+++
@@ -1,7 +0,0 @@
-extends RigidBody2D
-
-func doDamage(weapon: Node2D)->void:
-	current_hp -= weapon.damage
-	if current_hp <= 0:
-		# Here add explosions
-		queue_free()
--- Systems/global.gd
+++ Systems/global.gd
@@ -10,4 +10,4 @@ func _input(event: InputEvent)->void:
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().quit()
 	if Input.is_action_just_pressed("ui_reset"):
-		get_tree().reload_current_scene()
+		var reloaded = get_tree().reload_current_scene()
